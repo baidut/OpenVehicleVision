35ab5456891f638699bda980873e43222ce6535f
Zhenqiang.Ying <yingzhenqiang@gmail.com>
2016-02-01 17:52:16 +0800

    add vvThresh

---
+++ my_vehicle_vision_util/vvThresh.m
@@ -0,0 +1,24 @@
+classdef vvThresh
+%VVFEATURE do binarization
+% a color/grayscale image ---> a binary image
+%
+%   Example
+%   -------
+%   %  Call static methods. (otsu)
+%      colorImage = imread('K:\Documents\MATLAB\dataset\roma\BDXD54\IMG00006.jpg');
+%      grayImage = colorImage(:,:,1);
+%      BW = vvThresh.ostu(grayImage);
+%      imshow(BW);
+		
+    %% Public properties
+    % properties (GetAccess = public, SetAccess = private)
+    % end
+ 
+    %% Static methods
+    methods (Static)
+		function BW = ostu(GrayImg)
+		    BW = im2bw(GrayImg, graythresh(GrayImg));
+		end
+		
+    end% methods
+end% classdef
