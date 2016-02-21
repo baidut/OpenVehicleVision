47a8500a676c0b41a603b359a37455dbc01fc5d9
Zhenqiang.Ying <yingzhenqiang@gmail.com>
2016-02-21 21:10:14 +0800

    improve Ui

--- my_exp_util/Ui.m
+++ my_exp_util/Ui.m
@@ -3,95 +3,94 @@ classdef Ui
     %
     %   Example
     %   -------
-	%       I = imread('circuit.tif');
-	%      
-	%       subplot(121);
-	%       imshow(I);
-	%       image = struct('style','image','h',gca); % style must be lower case
-	%       thresh = struct('style','slider','min',0,'max',1,'value',0.5);
-	%       subplot(122);
-	%       % BW = edge(I,'sobel',THRESH)
-	%       Ui.imshow(gca, @edge, image, 'sobel', thresh); 
+    %       I = imread('circuit.tif');
+    %
+    %       subplot(121);
+    %       imshow(I);
+    %       image = struct('style','image','h',gca); % style must be lower case
+    %       thresh = struct('style','slider','min',0,'max',1,'value',0.5);
+    %       subplot(122);
+    %       % BW = edge(I,'sobel',THRESH)
+    %       Ui.imshow(gca, @edge, image, 'sobel', thresh);
     %
     %   Project website: https://github.com/baidut/openvehiclevision
     %   Copyright 2016 Zhenqiang Ying.
-	
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% Static methods
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    
+    
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    % Static methods
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
     methods (Static)
         function imshow(h, func, varargin)
-			cnt = 0;
+            cnt = 0;
             uictrls = cell(1,numel(varargin));
-			isuictrls = zeros(1,numel(varargin));
-			for n = 1:numel(varargin)
-				arg = varargin{n};
-				if isstruct(arg)
-					cnt = cnt + 1;
-					switch lower(arg.style)
-						case 'slider'
-							uictrls{cnt} = uicontrol('style','slider');
-						case 'popup'
-							uictrls{cnt} = uicontrol('style','popup');
-							
-						% additional ui controls
-						case 'image'
-							uictrls{cnt} = arg.h;
-						case 'rangeslider'
-						otherwise
-							disp('Unknown ui control style.');
-					end
-					% add other properties of uictrls{cnt}
-					switch lower(arg.style)
-						case {'slider','popup'}
-							isuictrls(cnt) = true;
-							fields = fieldnames(arg);
-							for n = 2:numel(fields) % style must be the first field
-								set(uictrls{cnt}, fields{n}, arg.(fields{n}));
-							end
-					end
-				end
-				% init uictrls done, add callback
-				for n = 1:cnt
-					if isuictrls(n)
-						set(uictrls{n},'callback',@(h,e)callback_func(uictrls,func,varargin{:}));
-					end
-				end
-				
-				% call once
-				callback_func(uictrls,func,varargin{:});
-			end
-			
-			function callback_func(uictrls,func,varargin)
-				
-				% load the value of uicontrols
-				idx = 0;
-				args = varargin;
-				for n = 1:numel(args)
-					arg = args{n};
-					if isstruct(arg)
-						idx = idx + 1;
-						switch lower(arg.style)
-							case {'popup','slider'}
-								args{n} = get(uictrls{idx},'value');
-							case 'image'
-								args{n} = getimage(uictrls{idx});
-							case 'rangeslider'
-							otherwise
-								disp('Unknown ui control style.');
-						end
-					end
-				end
-				result = func(args{:});
-				axes(h);
-				imshow(result);
-				
-				%use txt instead of title to avoid conflict
-				%TODO
-			end
-			
+            isuictrls = zeros(1,numel(varargin));
+            for n = 1:numel(varargin)
+                arg = varargin{n};
+                if isstruct(arg)
+                    cnt = cnt + 1;
+                    switch lower(arg.style)
+                        case 'slider'
+                            uictrls{cnt} = uicontrol('style','slider');
+                        case 'popup'
+                            uictrls{cnt} = uicontrol('style','popup');
+                            
+                            % additional ui controls
+                        case 'image'
+                            uictrls{cnt} = arg.h;
+                        case 'rangeslider'
+                        otherwise
+                            disp('Unknown ui control style.');
+                    end
+                    % add other properties of uictrls{cnt}
+                    switch lower(arg.style)
+                        case {'slider','popup'}
+                            isuictrls(cnt) = true;
+                            fields = fieldnames(arg);
+                            for n = 2:numel(fields) % style must be the first field
+                                set(uictrls{cnt}, fields{n}, arg.(fields{n}));
+                            end
+                    end
+                end
+                % init uictrls done, add callback
+                for n = 1:cnt
+                    if isuictrls(n)
+                        set(uictrls{n},'callback',@(h,e)callback_func()); 
+                    end
+                end
+                
+                % call once
+                callback_func();
+            end
+            
+            function callback_func()
+                % shared args: h,uictrls,func,varargin{:}
+                % load the value of uicontrols
+                idx = 0;
+                args = varargin;
+                for m = 1:numel(args)
+                    if isstruct(args{m})
+                        idx = idx + 1;
+                        switch lower(args{m}.style)
+                            case {'popup','slider'}
+                                args{m} = get(uictrls{idx},'value');
+                            case 'image'
+                                args{m} = getimage(uictrls{idx});
+                            case 'rangeslider'
+                            otherwise
+                                disp('Unknown ui control style.');
+                        end
+                    end
+                end
+                result = func(args{:});
+                axes(h);
+                imshow(result);
+                
+                %use txt instead of title to avoid conflict
+                %TODO
+            end
+            
         end
-
+        
     end% methods
 end% classdef
