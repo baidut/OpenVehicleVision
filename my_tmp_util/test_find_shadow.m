a72c8823ceb7a9beba7ea6c45807d9be883e9346
baidut <yingzhenqiang@gmail.com>
2016-03-08 14:29:25 +0800

    add adaptive threshold

--- my_tmp_util/test_find_shadow.m
+++ my_tmp_util/test_find_shadow.m
@@ -1,77 +1,150 @@
-function test_find_shadow
-imgFile = '%datasets\roma\BDXD54\IMG00164.jpg';
-% strong shadow IMG00002 IMG00071 IMG00146 (one side) IMG00164 (high light)
-% IMG00030 
-% weak shadow
-Raw = RawImg(imgFile);
-E = zeros([Raw.rows, Raw.cols]);
+function test_find_shadow(imgFile)
+% First do lane marking detection
+% novel: detect in shadow and non-shadow area
 
-% for r = 1:Raw.rows-1
-%     b1 = Raw.data(r,:,3);
-%     b2 = Raw.data(r+1,:,3);
-%     g1 = Raw.data(r,:,2);
-%     g2 = Raw.data(r+1,:,2);
-%     % max == b1
-%     E(r,(b1>=g1) & (b2<=g2) ) = 128;
-%     E(r,(b1<=g1) & (b2>=g2) ) = 255;
-% end
-%
-% for c = 1:Raw.cols-1
-%     b1 = Raw.data(:,c,3);
-%     b2 = Raw.data(:,c+1,3);
-%     g1 = Raw.data(:,c,2);
-%     g2 = Raw.data(:,c+1,2);
-%     % max == b1
-%     E((b1>=g1) & (b2<=g2),c ) = 128;
-%     E((b1<=g1) & (b2>=g2),c ) = 255;
-% end
+%% Params Setting
+
+% caltech_shaodw.png
+
+boundAngleRange = 30:75;%30:75;
+
+if nargin < 1
+    imgFile = '%datasets\roma\BDXD54\IMG00071.jpg'; %  '%datasets\roma\LRAlargeur26032003\IMG00579.jpg'; %
+end
+% strong shadow IMG00002 IMG00071 IMG00146 (one side) IMG00164 (high light)
+% IMG00030
+% weak shadow '%datasets\roma\LRAlargeur26032003\IMG00579.jpg'
+Raw = RawImg(imgFile);
+
+[R,G,B] = Raw.eachChn();
 
 %% Explore Strong Shadow Edge Feature
-% Conclusion: shadow edge occurs at delta B-G peak
-% IMPROVE: plot more in one figure.
-% select one column
-figure;
-imshow(rot90(Raw.data));%, 'Xdata',[1 Raw.cols]- 255);
-%%
-[~,G,B] = Raw.eachChn();
-hold on;
-show_column_gb(ceil(Raw.cols/2));
-show_column_gb(ceil(Raw.cols/4));
-show_column_gb(ceil(Raw.cols*3/4)); % basevalue is fixed for one axes, so `area` cannot be used
-camroll(-90);
+% test_gb;return
 
-%% Test shadow detection , shadow removal and ii iamge(shadow and light)
-figure;
-Mask = zeros([Raw.rows*Raw.cols, 3],class(Raw.data));% 'uint8'
-Mask(B>G&B<120,3) = 125;% Mask(B>G&B<120,:) = [0 0 255];
+%% Test strong shadow detection , shadow removal and ii iamge(shadow and light)
+% Note it is strong shadow detection!, no shadow detected in weak shadow
+% cases
+
+Mask = zeros([Raw.rows*Raw.cols, 3],'like',Raw.data);% 'uint8'
+LowerB = ~im2bw(B, graythresh(B));
+Shadow = (B>G&LowerB); %&B<80 adaptive thresh B
+Mask(Shadow,3) = 125;% Mask(B>G&B<120,:) = [0 0 255];
 Mask = reshape(Mask,[Raw.rows, Raw.cols, 3] );
-% imshow(Mask);
-imshow(imadd(Raw.data,Mask));
+
+% ----------------------------------------------------------------------- %
+figure;Fig.subimshow(Raw, imadd(Raw.data,Mask));return 
+
+SSEdge = bwperim(Mask(:,:,3)~=0); %bwboundaries(Mask, 8); % Strong Shadow Edge
+Mask = imoverlay(Mask, SSEdge, [255 255 0]);
+
+% figure;imshow(imadd(Raw.data,Mask));return
 
 %% For this time we don't need to do shadow removal, just remove the false edges
 % don't use shadow's boudary since it is variable to threshold
-% use B-G peak
 
-%% B-G peak
+%% Road Edge detection(no strong shadow edge)
+% vvEdge.testCanny(R);% don't use G or B to extract Edge
 
-%%
-    function show_column_gb(c)
-        %imshow( rot90(imoverlay(Raw.data,SelectedCol,[255 0 0])) ,'Ydata',[1 Raw.cols]- c);%, 'Xdata',[1 Raw.cols]- 255);
-        % imshow( imoverlay(Raw.data,SelectedCol,[255 0 0]) );
+S2 = vvFeature.S2(Raw.data);
+%% Dealing with large shadow: Edge detection in strong shadow area
+% in strong shadow area, cannot find edge in R image.
+% show hist and enhancement effects
+% in shadow area, the influence of false edge is very severe,
+% so instead of using edge, use region feature
+ShadowArea = R; %R;%vvFeature.S2(Raw.data);%R;
+
+ShadowArea(~Shadow) = 0;
+Enhanced = ShadowArea;
+Enhanced(Shadow) = histeq(ShadowArea(Shadow));
+% bw = im2bw(ShadowArea, graythresh(ShadowArea(Shadow)));
+
+% vvEdge.testCanny(Enhanced);return;
+EdgeInShadow = edge(Enhanced,'canny',[0.0200,0.2900],4.6323);
+BminsG = histeq(mat2gray(B - G));
+
+figure;Fig.subimshow(BminsG, Enhanced);return;% EdgeInShadow
+
+%% Edge in other area: using S2 to deal with weak shadow
+
+% vvEdge.testCanny(S2);return;
+EdgeInS2 = edge(S2,'canny',[0.0900,0.2000],6.5441);
+
+EdgeAll = EdgeInShadow | EdgeInS2;
+
+se = strel('square',5); %ceil(Raw.cols/100)*2+1);
+ErodedSSEdge = imdilate(SSEdge,se);
+EdgeTrue = EdgeAll & (~ErodedSSEdge);
+
+% use different color to show different kind of Edges
+figure;Fig.subimshow(EdgeInShadow, EdgeInS2);return; % EdgeAll,
+% figure;Fig.subimshow(ErodedSSEdge, EdgeTrue);return; % EdgeAll,
+
+%% Straight line Detection
+
+% detect many lines in one time
+BoundL = vvBoundModel.houghStraightLine(EdgeTrue, boundAngleRange);
+BoundR = vvBoundModel.houghStraightLine(EdgeTrue, -boundAngleRange); % -75:-30
+
+Result = Raw;
+f = Fig();f.subimshow(Raw,Result);
+BoundL.plot('r');
+BoundR.plot('g');
+
+% saveas(gcf, ['%Temp/', Raw.name, '.jpg']);
+% close(gcf);
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    function test_gb()
+        % Conclusion: strong shadow edge occurs at delta B-G peak
+        % don't support weak shadow
+        % IMPROVE: plot more in one figure.
+        % select one column
         
-        % plot(R(:,c), 1:Raw.rows, 'r');
-        % plot(G(:,c), 1:Raw.rows, 'g');
-        % plot(B(:,c), 1:Raw.rows, 'b');
-        %         diff = int32(B(:,c))-int32(G(:,c));
-        % plot(diff, 1:Raw.rows, 'b', 'MarkerFaceColor', 'b');
-        %area(double(B(:,c)')-double(G(:,c)'), 'FaceColor', 'y'); % area(int32(B(:,c))-int32(G(:,c))); %  'basevalue', -255
-                b = Raw.cols-c;
-                baseline = ones(1,Raw.rows)*b;
-                fill_between_lines = @(X,Y1,Y2,C) fill( [X fliplr(X)],  [Y1 fliplr(Y2)], C );
-                fill_between_lines( 1:Raw.rows, double(B(:,c)-G(:,c))'+b, baseline,'b');
-                fill_between_lines( 1:Raw.rows, -double(G(:,c)-B(:,c))'+b, baseline,'g');
-        % http://stackoverflow.com/questions/6245626/matlab-filling-in-the-area-between-two-sets-of-data-lines-in-one-figure
+        figure;
+        imshow(rot90(Raw.data));%, 'Xdata',[1 Raw.cols]- 255);
+        
+        hold on;
+        show_column_gb(ceil(Raw.cols/2));
+        show_column_gb(ceil(Raw.cols/4));
+        show_column_gb(ceil(Raw.cols*3/4)); % basevalue is fixed for one axes, so `area` cannot be used
+        camroll(-90);
+        
+        function show_column_gb(c)
+            b = Raw.cols-c;
+            baseline = ones(1,Raw.rows)*b;
+            fill_between_lines = @(X,Y1,Y2,C) fill( [X fliplr(X)],  [Y1 fliplr(Y2)], C );
+            fill_between_lines( 1:Raw.rows, 2*double(B(:,c)-G(:,c))'+b, baseline,'b');
+            fill_between_lines( 1:Raw.rows, -2*double(G(:,c)-B(:,c))'+b, baseline,'g');
+            % http://stackoverflow.com/questions/6245626/matlab-filling-in-the-area-between-two-sets-of-data-lines-in-one-figure
+        end
     end
+
+    function edge_detection_by_column_scan()
+        
+        E = zeros([Raw.rows, Raw.cols]);
+        
+        for r = 1:Raw.rows-1
+            b1 = Raw.data(r,:,3);
+            b2 = Raw.data(r+1,:,3);
+            g1 = Raw.data(r,:,2);
+            g2 = Raw.data(r+1,:,2);
+            % max == b1
+            E(r,(b1>=g1) & (b2<=g2) ) = 128;
+            E(r,(b1<=g1) & (b2>=g2) ) = 255;
+        end
+        
+        for c = 1:Raw.cols-1
+            b1 = Raw.data(:,c,3);
+            b2 = Raw.data(:,c+1,3);
+            g1 = Raw.data(:,c,2);
+            g2 = Raw.data(:,c+1,2);
+            % max == b1
+            E((b1>=g1) & (b2<=g2),c ) = 128;
+            E((b1<=g1) & (b2>=g2),c ) = 255;
+        end
+    end
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
 end
 % % select a rect region
 % rect = ceil([601.30519322064 488.665679488886 14.3181818181818 14.0454545454545]);
