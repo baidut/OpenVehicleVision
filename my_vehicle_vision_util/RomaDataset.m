331fae44f7b992e0144ca01fa19bcdf658feb554
baidut <yingzhenqiang@gmail.com>
2016-03-11 11:18:31 +0800

    update RomaDataset.groundTruth

--- my_vehicle_vision_util/RomaDataset.m
+++ my_vehicle_vision_util/RomaDataset.m
@@ -52,106 +52,169 @@ classdef RomaDataset < vvDataset
             % figure,montage(imgs1);          % all
             % figure,montage(imgs2(1:2:end)); % partial
             % figure,montage(imgs3(end:-1:1));% descend
-            % 
+            %
             % % Get all images
             % imgs = roma.images(roma.situations,roma.scenarioMap.values());
             
             if ~iscell(situation), situation = {situation}; end
             if ~iscell(scenario), scenario = {scenario}; end
             
-			situ = repmat(situation, [1 numel(scenario)]);
-			scen = repmat(scenario, [1 numel(situation)]);
+            situ = repmat(situation, [1 numel(scenario)]);
+            scen = repmat(scenario, [1 numel(situation)]);
             files = cellfun(@getAndLoadMov,situ,scen,'UniformOutput',false);
             images = cat(1, files{:});
-			
-			function imageList = getAndLoadMov(situation, scenario)
-				folder = fullfile(roma.path,situation);
+            
+            function imageList = getAndLoadMov(situation, scenario)
+                folder = fullfile(roma.path,situation);
                 movFile = fullfile(folder,scenario);
                 imageList = strcat(folder,filesep,roma.loadmov(movFile));
-			end
+            end
         end
-		
-		function [time] = benchmark(roma, algo, images, threshRange)
-		% images = roma.images('BDXD54','Rimg.mov');
-		% roma.benchmark(@vvMark.LT, images, [1 254])
-		% 
-		% image = fullfile(roma.path,'BDXD54\IMG00002.jpg');
-		% roma.benchmark(@vvMark.LT, image);
-			if nargin < 4
-				threshRange = 1:254; % exclude 0 and 255
-			end
-		
-			if ~iscell(images), images = {images}; end
-			
-			% for im = images
-				% image = im{1};
-				% fun = @(t)algo(image,t);
-				% results = arrayfun(fun, threshRange,'UniformOutput',false);
-			% end
-			
-			imgData = cellfun(@imread,'UniformOutput',false);
-			param1 = repmat(imgData,numel(threshRange));
-			param2 = repmat(threshRange,numel(images));
-			
-			tic;
-			results = cellfun(algo,param1,param2,'UniformOutput',false);
-			time = toc/numel(images);
-			
-			% benchmark 
-			% gtImgs = roma.groundTruth(images);
-			
-			if nargout == 0 
-			% ask if visualize best results
-			% ask if save results
-			end
-			
-			
-		end
-		
-% 		function evalResult(roma, )
-% 		
-% 		end
-% 		
-% 		function compareResult(roma, )
-% 		
-% 		
-% 		end
-		
-		function benchmarkAll(roma,algo)
-		% benchmark for all images, all threshold
-		% and output a text file report.
-		
-			imgs = roma.images(roma.situations,roma.scenarioMap.values());
-			benchmark(algo, images);
-		end
-		
-		function comparePerformance(roma,algoList)
-		
-		% See also kitti road benchmark http://www.cvlibs.net/datasets/kitti/eval_road.php
-			algoName = cellfun(@char, algoList, 'UniformOutput', false);
-			cellfun(@roma.benchmarkAll, algoList, 'UniformOutput', false);
-			
-			% KITTI: MaxF,AP,PRE,REC,TN,FN,FPR,FNR,Runtime,Environment
-			% MaxF 
-			% AP
-			ACC = (TP + TN) / (TP + TN + FP + FN); % Accuracy (ACC)
-			PRE = TP / (TP + FP); % Precision
-			REC = TP / (TP + FN); % Recall
-			FPR = FP / (FP + TN); % fall-out or false positive rate (FPR)
-			FNR = FN / (FN + TP); % miss rate or false negative rate (FNR)
-			% Runtime = time ./ n_images; 
-			% to check your environment, run `msinfo32` in windows
-			Environment = repmat('4 cores @ 3.4 Ghz (Matlab)', [1 n_algo]); 
-			
-			% output text file report
-			T = table(ACC,PRE,REC,TN,FN,FPR,FNR,Environment, ...
-				'RowNames',algoName);
-			%
-			% plot ROC and DSC curve for each algo
-		end
     end
     
     methods (Static)
+        function [TPs,FPs,TNs,FNs, time] = benchmark(algo, imageFiles, threshRange)
+            % images = roma.images('BDXD54','Rimg.mov');
+            % roma.benchmark(@vvMark.LT, images, [1 254])
+            %
+            % image = fullfile(roma.path,'BDXD54\IMG00002.jpg');
+            % roma.benchmark(@vvMark.LT, image);
+            if nargin < 4
+                threshRange = 1:254; % exclude 0 and 255
+            end
+            
+            if ~iscell(imageFiles), imageFiles = {imageFiles}; end
+            
+            % for im = images
+            % image = im{1};
+            % fun = @(t)algo(image,t);
+            % results = arrayfun(fun, threshRange,'UniformOutput',false);
+            % end
+            
+            images = cellfun(@imread,imageFiles,'UniformOutput',false);
+			gtImgs = cellfun(@imread,RomaDataset.groundTruth(imageFiles),'UniformOutput',false);
+			
+            param1 = repmat(images,numel(threshRange));
+            param2 = repmat(threshRange,numel(images));
+            
+            tic;
+            results = cellfun(algo,param1,param2,'UniformOutput',false);
+            time = toc/numel(imageFiles);
+            
+            % benchmark
+            [TPs,FPs,TNs,FNs] = RomaDataset.compareWithGroundTruth(results, gtImgs);
+            
+            if nargout == 0
+                % ask if visualize best results
+                % ask if save results
+            end
+        end
+        
+        function [TPs,FPs,TNs,FNs,cMask] = compareWithGroundTruth(results, GTs)
+		% roma = RomaDataset('%datasets\roma');
+		% imageFile = fullfile(roma.path, 'RouenN8IRC051900\IMG00007.jpg');
+		% I = imread(imageFile);
+		% GT = imread(roma.groundTruth(imageFile));
+		% Result = vvMark.LT(I, 30); % threshold:30
+		% roma.compareWithGroundTruth(Result, GT);
+		
+            if ~iscell(results), results = {results}; end
+            [cTP,cFP,cTN,cFN,cMask] = cellfun(@eval,results,GTs,'UniformOutput',false);
+            TPs = cell2mat(cTP);
+            FPs = cell2mat(cFP);
+            TNs = cell2mat(cTN);
+            FNs = cell2mat(cFN);
+            
+            function [TP,FP,TN,FN,visualizeMask] = eval(result, GT)
+			% Note TP... is not a ratio but a count number about
+			% how many true positive pixels in image or images.
+			
+                % lane-marking    GT == 255   result == 1
+                % special-marking GT == 125   result == 0
+                % non-marking     GT == 0     result == 0
+                TP_bw = (result == 1) & (GT == 255);
+                FP_bw = (result == 1) & (GT ~= 255);
+                TN_bw = (result == 0) & (GT ~= 255);
+                FN_bw = (result == 0) & (GT == 255);
+				
+				TP = sum(TP_bw(:));
+				FP = sum(FP_bw(:));
+				TN = sum(TN_bw(:));
+				FN = sum(FN_bw(:));
+                % red denotes false negatives, blue areas correspond to
+                % false positives and green represents true positives.
+                % FP and FN need to be transparent for finding the
+                % possible reason.
+                % -------------------------- R ---------- G ------- B -----
+                visualizeMask = uint8(cat(3,FN_bw*128,TP_bw*255,FP_bw*128));
+            end
+        end
+        %
+        function [TPs,FPs,TNs,FNs] = compareTwo(results1, results2)
+			assert(numel(results1)==numel(results2));
+			if ~iscell(results1), 
+				results1 = {results1};
+			end
+			
+            GT = roma.groundTruth(results1);
+            [cTP,cFP,cTN,cFN] = cellfun(@eval,results,GT);
+            TPs = cell2mat(cTP);
+            FPs = cell2mat(cFP);
+            TNs = cell2mat(cTN);
+            FNs = cell2mat(cFN);
+            
+            function [TP,FP,TN,FN] = eval(result, gt)
+			% Note TP... is not a ratio but a count number about
+			% how many true positive pixels in image or images.
+			
+                % lane-marking    gt == 255   result == 1
+                % special-marking gt == 125   result == 0
+                % non-marking     gt == 0     result == 0
+                TP_bw = (result == 1) & (gt == 255);
+                FP_bw = (result == 1) & (gt ~= 255);
+                TN_bw = (result == 0) & (gt ~= 255);
+                FN_bw = (result == 0) & (gt == 255);
+				
+				TP = sum(TP_bw(:));
+				FP = sum(FP_bw(:));
+				TN = sum(TN_bw(:));
+				FN = sum(FN_bw(:));
+            end
+        end
+        
+        function benchmarkAll(algo)
+            % benchmark for all images, all threshold
+            % and output a text file report.
+            images = RomaDataset.images(RomaDataset.situations,RomaDataset.scenarioMap.values());
+            benchmark(algo, images);
+            % output a text file report
+        end
+        
+        function comparePerformance(roma,algoList)
+            
+            % See also kitti road benchmark http://www.cvlibs.net/datasets/kitti/eval_road.php
+            algoName = cellfun(@char, algoList, 'UniformOutput', false);
+            cellfun(@roma.benchmarkAll, algoList, 'UniformOutput', false);
+            
+            % KITTI: MaxF,AP,PRE,REC,TN,FN,FPR,FNR,Runtime,Environment
+            % MaxF
+            % AP
+            ACC = (TP + TN) / (TP + TN + FP + FN); % Accuracy (ACC)
+            PRE = TP / (TP + FP); % Precision
+            REC = TP / (TP + FN); % Recall
+            FPR = FP / (FP + TN); % fall-out or false positive rate (FPR)
+            FNR = FN / (FN + TP); % miss rate or false negative rate (FNR)
+            % Runtime = time ./ n_images;
+            % to check your environment, run `msinfo32` in windows
+            Environment = repmat('4 cores @ 3.4 Ghz (Matlab)', [1 n_algo]);
+            
+            % output text file report
+            T = table(ACC,PRE,REC,TN,FN,FPR,FNR,Environment, ...
+                'RowNames',algoName);
+            %
+            % plot ROC and DSC curve for each algo
+        end
+        
         function files = loadmov(movFile)
             %RomaDataset.loadlist output full file name
             %loadlist provided by roma will lose ext.
@@ -163,13 +226,18 @@ classdef RomaDataset < vvDataset
             % all the names
             files=data(2:nelem+1);
         end
-		
-		function gtFiles = groundTruth(oriFiles)
-		% IMG00007.jpg
-		% RIMG00007.pgm
-			if ~iscell(oriFiles), oriFiles = {oriFiles}; end
-			func = @(f) [f(1:end-12) 'R' f(end-11:end-4) '.pgm'];
-			gtFiles = cellfun(func,oriFiles,'UniformOutput',false);
-		end
+        
+        function gtFiles = groundTruth(oriFiles)
+            % IMG00007.jpg
+            % RIMG00007.pgm
+            
+            func = @(f) [f(1:end-12) 'R' f(end-11:end-4) '.pgm'];
+            
+            if ~iscell(oriFiles) % one single image
+                gtFiles = func(oriFiles);
+            else
+                gtFiles = cellfun(func,oriFiles,'UniformOutput',false);
+            end
+        end
     end
 end
