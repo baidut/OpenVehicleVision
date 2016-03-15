ef1ada921b2b90e04842abdbf3b74ac78d891dfb
baidut <yingzhenqiang@gmail.com>
2016-03-15 20:41:49 +0800

    road face extraction done. to do test in sunny dataset

--- main/dualLaneDetector.m
+++ main/dualLaneDetector.m
@@ -1,68 +1,106 @@
 classdef dualLaneDetector<handle
-% foreach_file_do('E:\Documents\pku-road-dataset\1\EMER0009\0*.jpg', @dualLaneDetector);
-%{
+    % foreach_file_do('E:\Documents\pku-road-dataset\1\EMER0009\0*.jpg', @dualLaneDetector);
+    %{
 AfterRain = vvDataset('%datasets\nicta-RoadImageDatabase\After-Rain');
 files = AfterRain.filenames('*.tif');
  cellfun(@dualLaneDetector, files, 'UniformOutput',false);
-%}
-% 
-% 0720
-
+    %}
+    %
+    % 0720
+    
     %% Public properties
     properties (GetAccess = public, SetAccess = private)
     end
     
     methods (Access = public)
         % 'F:\Documents\pku-road-dataset\1\EMER0009\0379.jpg'
-        function obj = dualLaneDetector(imgFile)
+        function self = dualLaneDetector(imgFile)
+            %% settings
+            med_size = [10 10];
+            
             Raw = RawImg(imgFile);%0289
             %TODO: below the horizon
+            
+            %% ROI selection
             ROI = Raw.rectroi({ceil(Raw.rows/2):Raw.rows,1:Raw.cols});
+            
+            %% Shadow Removal
+            ShadowFreeImage = self.rgb2ii(ROI);
+            GraySmooth = medfilt2(ShadowFreeImage,med_size);
+            %% Segmentation
+            Bw = vvThresh.otsu(GraySmooth);
+%             Bw = im2bw(GraySmooth, graythresh(GraySmooth)+0.15); 
+            %% Remove Noise
+            % imguidedfilter: bad performance
+            %RoadSmooth = imguidedfilter(Road);
+            %imshowpair(Road,RoadSmooth,'montage');
+            
+            %% just get the max conn area
+            %RoadSmooth = bwareaopen(~Road, 50, 8);
+            %RoadSmooth = bwareaopen(Road, 500, 8);% BwImg.dilate(Road);
+            RoadSmooth = medfilt2(Bw,med_size);
+            RoadFace = BwImg.maxarea(RoadSmooth);
+            RoadBound = BwImg.bound(RoadFace);
+            
+            
+            %% line detection
+            boundAngleRange = 30:75;
+            
+            BoundL = vvBoundModel.houghStraightLine(RoadBound, boundAngleRange); % 0:89
+            BoundR = vvBoundModel.houghStraightLine(RoadBound, -boundAngleRange); % -89:0
+            
+            Result = Raw.roidrawmask(RoadFace); % Img + mask (+g, -b, -r) use mean
+            Fig.subimshow(Raw,Result,ROI, RoadFace);
+            selplot(2);
+            %plotpoint(Edge);% TODO: remove plotpoint,
+            BoundL.plot('r');
+            BoundR.plot('g');
+            
+%             saveas(gcf, ['%Temp/', Raw.name, '.jpg']);
+%             close(gcf);
+            
+            
+%             Fig.subimshow(Raw, ShadowFreeImage, GraySmooth, Bw, RoadArea, RoadBound);
+%             maxfig;
+            return;
             
             %% Preproc:Filtering road marking
             % may smooth the road boundary either
             
             % do filter on R, G, B then cat
             % img.eachchn()
-%             LT = vvMark.rowFilter(ROI, @vvMark.LT);
-%             MLT = vvMark.rowFilter(ROI, @vvMark.MLT);
-%             SMLT = vvMark.rowFilter(ROI, @vvMark.SMLT);
-%             Fig.subimshow(ROI,LT,MLT,SMLT);
+            %             LT = vvMark.rowFilter(ROI, @vvMark.LT);
+            %             MLT = vvMark.rowFilter(ROI, @vvMark.MLT);
+            %             SMLT = vvMark.rowFilter(ROI, @vvMark.SMLT);
+            %             Fig.subimshow(ROI,LT,MLT,SMLT);
             
-%             Size = Uiview('slider','min',0,'max',1,'value',0.1);
-%             SMLT = Uictrl(@vvMark.rowFilter, ROI, @vvMark.SMLT, Size);
-%             Fig.subimshow(ROI,SMLT);
-
-%             SMLT = vvMark.rowFilter(ROI,@vvMark.SMLT,0.3);
-%             ROI = SMLT;
+            %             Size = Uiview('slider','min',0,'max',1,'value',0.1);
+            %             SMLT = Uictrl(@vvMark.rowFilter, ROI, @vvMark.SMLT, Size);
+            %             Fig.subimshow(ROI,SMLT);
+            
+            %             SMLT = vvMark.rowFilter(ROI,@vvMark.SMLT,0.3);
+            %             ROI = SMLT;
             
             %% Segmentation
-%             vvSeg.felzen(ROI);return;
+            %             vvSeg.felzen(ROI);return;
             ISeg = vvSeg.felzen(ROI,3,200,50);%200
             % too mush sigma will loose small details
-            % sigma bigger smooth, k smaller 
+            % sigma bigger smooth, k smaller
             RoadFace = ISeg.maxarea();
             
             %% Road Bound Edge
             RoadBound = RoadFace.bound(8);
             % implot(ROI, ISeg, RoadFace, imoverlay(ROI, RoadBound.data, [255, 255, 0]));
             % return;
-            %% line detection
-            Edge = RoadBound.data;
-            boundAngleRange = 30:75;
             
-            BoundL = vvBoundModel.houghStraightLine(Edge, boundAngleRange); % 0:89
-            BoundR = vvBoundModel.houghStraightLine(Edge, -boundAngleRange); % -89:0
-            
-            Result = Raw.roidrawmask(RoadFace.data);
-            Fig.subimshow(Raw,Result,ROI,ISeg);
-            selplot(2);
-            %plotpoint(Edge);% TODO: remove plotpoint, 
-            BoundL.plot('r');
-            BoundR.plot('g');
-            
-            saveas(gcf, ['%Temp/', Raw.name, '.jpg']); 
-            close(gcf);
+        end
+    end
+    %% Algorithm that can be used in the future
+    methods (Static)
+        function ii_image = rgb2ii(image)
+            [~, G, B] = getChannel(im2double(image));
+            ii_image =  1-(G+0.2-B)./B;
+            ii_image(ii_image<0) = 0;
         end
     end
 end
--- my_img_util/BwImg.m
+++ my_img_util/BwImg.m
@@ -20,12 +20,7 @@ classdef BwImg
             h = imshow(obj.data, varargin{:});
         end
         
-        function bwImg = bound(obj, varargin)
-            bw = bwperim(obj.data, varargin{:});
-            bw([1,end],:) = 0;
-            bw(:,[1,end]) = 0;
-            bwImg = BwImg(bw);
-        end
+        
         
         function colorImg = tocolor(obj, color)
             % Do not support multi-channel image
@@ -34,6 +29,56 @@ classdef BwImg
             end
             colorImg = cat(3, obj.data*color(1), obj.data*color(2), obj.data*color(3));
             colorImg = ColorImg(colorImg);
+        end
+        %% get value
+        function value = uplus(obj)
+            value = obj.data;
+        end
+    end
+    methods (Static)
+        
+        function bw = bound(obj, varargin)
+            bw = bwperim(+obj, varargin{:});
+            bw([1,end],:) = 0;
+            bw(:,[1,end]) = 0;
+        end
+        
+        % test case
+        % I = BwImg(imread('text.png'));
+        % imshow(I.maxarea(+I));
+        % 
+        function bw = maxarea(obj) % input can be an obj or bw image.
+        % http://cn.mathworks.com/help/images/ref/bwconncomp.html
+            bw = false(size(+obj));
+            CC = bwconncomp(+obj); % support BwImg
+
+            numPixels = cellfun(@numel,CC.PixelIdxList);
+            [~,idx] = max(numPixels);
+            bw(CC.PixelIdxList{idx}) = 1;
+
+		% max connected area
+        % http://stackoverflow.com/questions/22514668/select-largest-object-in-an-image
+        % labeled image
+		% http://stackoverflow.com/questions/20725603/how-to-select-the-object-with-the-largest-area
+% 			L = bwlabel(+obj, 4);
+%             
+%             % call label image's maxarea
+%             stat = regionprops(L,'Centroid','Area','PixelIdxList');
+% 			[~,index] = max([stat.Area]);
+%             
+% 			bw = (+obj == index);
+        end
+        % for object
+        % obj.data = obj.maxarea();
+        
+        function bw = dilate(obj)
+            se = strel('ball',5,5);
+            bw = imdilate(+obj, se);
+        end
+        
+        function bw = erode(obj)
+            se = strel('ball',5,5);
+            bw = im(+obj, se);
         end
     end% methods
 end% classdef
