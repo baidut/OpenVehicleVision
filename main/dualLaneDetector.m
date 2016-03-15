00db72dfddf2c2ffb5442fe7439311db6dd259b7
baidut <yingzhenqiang@gmail.com>
2016-03-15 22:25:08 +0800

    basic road face detection and lane marking detection done. todo: curve cases

--- main/dualLaneDetector.m
+++ main/dualLaneDetector.m
@@ -22,14 +22,15 @@ files = AfterRain.filenames('*.tif');
             %TODO: below the horizon
             
             %% ROI selection
-            ROI = Raw.rectroi({ceil(Raw.rows/2):Raw.rows,1:Raw.cols});
+            rHorizon = ceil(Raw.rows/2);
+            ROI = Raw.rectroi({rHorizon:Raw.rows,1:Raw.cols});
             
             %% Shadow Removal
             ShadowFreeImage = self.rgb2ii(ROI);
             GraySmooth = medfilt2(ShadowFreeImage,med_size);
             %% Segmentation
             Bw = vvThresh.otsu(GraySmooth);
-%             Bw = im2bw(GraySmooth, graythresh(GraySmooth)+0.15); 
+            %             Bw = im2bw(GraySmooth, graythresh(GraySmooth)+0.15);
             %% Remove Noise
             % imguidedfilter: bad performance
             %RoadSmooth = imguidedfilter(Road);
@@ -40,6 +41,7 @@ files = AfterRain.filenames('*.tif');
             %RoadSmooth = bwareaopen(Road, 500, 8);% BwImg.dilate(Road);
             RoadSmooth = medfilt2(Bw,med_size);
             RoadFace = BwImg.maxarea(RoadSmooth);
+            RoadFace = imfill(RoadFace,'holes');
             RoadBound = BwImg.bound(RoadFace);
             
             
@@ -50,18 +52,38 @@ files = AfterRain.filenames('*.tif');
             BoundR = vvBoundModel.houghStraightLine(RoadBound, -boundAngleRange); % -89:0
             
             Result = Raw.roidrawmask(RoadFace); % Img + mask (+g, -b, -r) use mean
-            Fig.subimshow(Raw,Result,ROI, RoadFace);
-            selplot(2);
+            
+            Marking = dualLaneDetector.getLaneMarking(ROI, RoadFace);
+            
+            line = vvBoundModel.houghStraightLine(Marking, -70:70);
+            
+           
+            
+            %% Display results
+            Fig.subimshow(Raw,Result,RoadFace, Marking); % Marking RoadFace
+            selplot(1);
             %plotpoint(Edge);% TODO: remove plotpoint,
-            BoundL.plot('r');
-            BoundR.plot('g');
+            BoundL.plot('r', 'LineWidth' , 5);
+            BoundR.plot('g', 'LineWidth' , 5);
             
-%             saveas(gcf, ['%Temp/', Raw.name, '.jpg']);
-%             close(gcf);
+            if isempty(line)
+                disp('Fail in lane markings detection.');
+            else
+                nRow = size(ROI,1);
+                PointS = [line.row(1), 1]; % start point
+                PointE = [line.row(nRow), nRow]; % end point
+                lineMarking = LineObj(PointS, PointE);
+                
+                % Plot
+                lineMarking.plot('LineWidth',3,'Color','blue');
+            end
+            
+            %             saveas(gcf, ['%Temp/', Raw.name, '.jpg']);
+            %             close(gcf);
             
             
-%             Fig.subimshow(Raw, ShadowFreeImage, GraySmooth, Bw, RoadArea, RoadBound);
-%             maxfig;
+            %             Fig.subimshow(Raw, ShadowFreeImage, GraySmooth, Bw, RoadArea, RoadBound);
+            %             maxfig;
             return;
             
             %% Preproc:Filtering road marking
@@ -101,6 +123,55 @@ files = AfterRain.filenames('*.tif');
             [~, G, B] = getChannel(im2double(image));
             ii_image =  1-(G+0.2-B)./B;
             ii_image(ii_image<0) = 0;
+        end
+        
+        function Marking = getLaneMarking(ROI, roadFaceMask)
+            % roi is a rgb image
+            % roadFaceMask is a bw (1: road, 0: non-road)
+            [nRow, nCol, ~] = size(ROI);
+            V_ROI = im2double(max(ROI, [], 3)); % Note!
+            I2 = zeros(nRow, nCol);
+            
+            s_max = 50;
+            
+            for r = 1 : nRow
+                mw = ceil(s_max * r / nRow); % marking width
+                for c =  (1 + 5*mw):(nCol-5*mw)
+                    I2(r, c) = 2*V_ROI(r,c) - (V_ROI(r,c-mw) + V_ROI(r,c+mw)) ...
+                        - abs(V_ROI(r,c-mw) - V_ROI(r,c+mw));
+                end
+            end
+            
+            if nargin > 1
+                I2(~roadFaceMask) = 0;
+                Marking = im2bw(I2, graythresh(I2(roadFaceMask)));
+            else
+                Marking = im2bw(I2, graythresh(I2(I2~=0)));
+            end
+        end
+        
+        function dtLaneMarking(Img,rHorizon,cBoundaryL,cBoundaryR,thetaSet)
+            
+            %%  Region of interest adjustment
+            %ROI = Img(rHorizon:end,:,1);
+            ROI = Img(rHorizon:end,:,:);
+            
+            %%  Lane-marking feature extraction
+            Marking = dualLaneDetector.getLaneMarking(ROI,cBoundaryL,cBoundaryR);
+            
+            %% Lane model fitting
+            line = vvBoundModel.houghStraightLine(Marking, thetaSet);
+            
+            if isempty(line)
+                disp('Fail in lane markings detection.');
+            else
+                PointS = [line.row(1), rHorizon]; % start point
+                PointE = [line.row(nRow), nRow+rHorizon]; % end point
+                lineMarking = LineObj(PointS, PointE);
+                
+                % Plot
+                lineMarking.plot('LineWidth',3,'Color','red');
+            end
         end
     end
 end
