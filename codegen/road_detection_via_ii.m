6fbb194ffa5ab074e52e0509c0ff622b1719f671
baidut <yingzhenqiang@gmail.com>
2016-03-28 18:39:32 +0800

    add road_detection_via_ii.m

--- codegen/road_detection_via_ii.m
+++ codegen/road_detection_via_ii.m
@@ -1,16 +1,17 @@
-function road_detection_via_ii(imgFile, ii_b)
+function roadFace = road_detection_via_ii(imgFile, ii_b)
 rawImg = imread(imgFile);
-[nRow, nCol, ~] = size(rawImg);
+[nRow, ~, ~] = size(rawImg);
 % 1000 10
 % 480  4
 szFilter = [8 8]; %[8 8]; %ones([1 2])*ceil(nCol/10);
 rHorizon = ceil(nRow/2);
+% otsu is very unstable when there are sky (very high gray value)
 
-roiImg = rawImg(rHorizon:end,:,:);
+% roiImg = rawImg(rHorizon:end,:,:);
 
 %% RGB --> II
-G = roiImg(:,:,2);
-B = roiImg(:,:,3);
+G = rawImg(:,:,2);
+B = rawImg(:,:,3);
 % im2double 0~1
 iiImg =  2 - (im2double(G+ii_b))./(im2double(B)+eps);
 % iiImg(iiImg<0)=0;
@@ -24,7 +25,9 @@ iiImg =  2 - (im2double(G+ii_b))./(im2double(B)+eps);
 smoothImg = wiener2(iiImg, szFilter);
 
 %%
-bw = im2bw(smoothImg, graythresh(smoothImg));
+bw = im2bw(smoothImg, graythresh(smoothImg(rHorizon:end,:,:)));
+
+
 bwSmooth = medfilt2(bw, [5 5]); % first use wiener2 then use medfilt2
 bwEroded =  imopen(bwSmooth, strel('disk',8,8)); %imerode(bwSmooth, strel('disk',4,4));
 
@@ -38,13 +41,16 @@ maxConnected(CC.PixelIdxList{idx}) = 1;
 
 roadFace = imfill(maxConnected,'holes');
 
-roadFace = [zeros([rHorizon-1 nCol]); roadFace];
-% imshow(roadFace)
-% return;
-[name, ext] = filename(imgFile);
-imwrite(roadFace, [name '_road.' ext]);
 
-% Fig.subimshow(rawImg, iiImg, smoothImg, bw, bwEroded, roadFace);
+%% Debug
+% roadFace = [zeros([rHorizon-1 nCol]); roadFace];
+Fig.subimshow(rawImg, iiImg, smoothImg, bw, bwEroded, roadFace);
+
+if nargout == 0
+    [name, ext] = filename(imgFile);
+    imwrite(roadFace, [name '_road.' ext]);
+end
+
 end
 
 function [name, ext] = filename(file)
--- my_vehicle_vision_util/vvFile.m
+++ my_vehicle_vision_util/vvFile.m
@@ -8,7 +8,7 @@ classdef vvFile  < handle
 % vvFor.each(Imgs, @imshow); % cell array
 % vvFor.each(Imgs, @(im) {} );
 
-I = files{i}
+% I = files{i}
 
     properties (GetAccess = public, SetAccess = private)
 		results % cell array to save result
@@ -18,6 +18,15 @@ I = files{i}
     methods (Static)
         function filename = name(file) 
 			[~,filename,~] = fileparts(file);
+        end
+        
+        function path = path(file) 
+			[path,~,~] = fileparts(file);
+        end
+        
+        function path_name = pn(file) 
+			[path,name,~] = fileparts(file);
+            path_name = [path,'\', name];
         end
     end
 	
